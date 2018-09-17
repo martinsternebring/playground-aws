6f4f166bc5d5872767cf8fa8543d2e2959324472
Martin <martin.sternebring@gmail.com>
2018-09-17 23:19:34 +0200

    Moving forward...

--- go/cards/main.go
+++ go/cards/main.go
@@ -2,7 +2,11 @@ package main
 
 func main() {
 	cards := newDeck()
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	cards.shuffle()
+	cards.print()
+
+	//--Read write from file--
+	//cards.saveToFile("my_cards")
+	//cards := newDeckFromFile("my_cards")
+
 }
