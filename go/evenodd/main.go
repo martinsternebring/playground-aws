888de4754210ab25cc92b62bb42344bd96b1f869
Martin <martin.sternebring@gmail.com>
2018-09-17 23:30:59 +0200

    Even odd test

---
+++ go/evenodd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	n := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, num := range n {
+		if num%2 == 0 {
+			fmt.Println("Even")
+		} else {
+			fmt.Println("Odd")
+		}
+	}
+}
